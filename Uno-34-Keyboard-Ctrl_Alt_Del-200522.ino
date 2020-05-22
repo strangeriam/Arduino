5542c126e9419b47f8f21f3a9f51111c43cfdd3b
Rlulu <64244191+strangeriam@users.noreply.github.com>
2020-05-22 16:12:23 +0800

    Add files via upload

---
+++ Uno-34-Keyboard-Ctrl_Alt_Del-200522.ino
@@ -0,0 +1,62 @@
+/* Button for Keyboard (Arduino Leonardo only) */
+/*
+ Button
+ Turns on and off a light emitting diode(LED) connected to digital  
+ pin 13, when pressing a pushbutton attached to pin 2. 
+ 
+ The circuit:
+ * LED attached from pin 13 to ground 
+ * pushbutton attached to pin 2 from +5V
+ * 10K resistor attached to pin 2 from ground
+ * Note: on most Arduinos there is already an LED on the board attached to pin 13.
+ */
+
+#include "Keyboard.h"
+
+// constants won't change. They're used here to 
+// set pin numbers:
+const int buttonPin = 2;            // the number of the pushbutton pin
+const int ledPin =  13;             // the number of the LED pin
+const char ctrlKey = KEY_LEFT_CTRL; // for windows
+const char altKey = KEY_LEFT_ALT;   // for windows
+const char delKey = KEY_DELETE;     // for windows
+// variables will change:
+int buttonState = 0;                // variable for reading the pushbutton status
+
+void setup() {
+      // initialize the LED pin as an output:
+      pinMode(ledPin, OUTPUT);      
+
+      // initialize the pushbutton pin as an input:
+      pinMode(buttonPin, INPUT);
+      digitalWrite(buttonPin, HIGH);
+
+      Keyboard.begin();
+}
+
+void loop(){
+      // read the state of the pushbutton value:
+      buttonState = digitalRead(buttonPin);
+
+      // check if the pushbutton is pressed.
+      // if it is, the buttonState is HIGH:
+      if (buttonState == LOW) {     
+            // turn LED on:    
+            digitalWrite(ledPin, HIGH); 
+            delay(30);
+
+            while(buttonState == LOW){
+                  buttonState = digitalRead(buttonPin);
+                  Keyboard.press(ctrlKey);
+                  Keyboard.press(altKey);
+                  Keyboard.press(delKey);
+            }
+      } 
+      else {
+            // turn LED off:
+            digitalWrite(ledPin, LOW); 
+            Keyboard.releaseAll();
+      }
+
+      delay(100);
+}
